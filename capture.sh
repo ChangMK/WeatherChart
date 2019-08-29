572969623d6ebef07049172e0dd0156512c645e3
MK Chang <mchang@console-log>
2019-08-29 11:05:15 +0800

    added radar return chart

--- capture.sh
+++ capture.sh
@@ -7,6 +7,7 @@
 
 i=0
 j=0
+LOCAL="/home/mk/DD2/weatherChartDaily/"
 PICTURETYPE=".jpg"
 USERAGENT="Mozilla/5.0"
 TIMESTAMP=$(date +"%s")
@@ -18,24 +19,33 @@ VISTRGB="https://www.cwb.gov.tw/Data/satellite/LCC_VIS_TRGB_2750/LCC_VIS_TRGB_27
 IR1CR="https://www.cwb.gov.tw/Data/satellite/LCC_IR1_CR_2750/LCC_IR1_CR_2750"
 #色調強化
 IR1MB="https://www.cwb.gov.tw/Data/satellite/LCC_IR1_MB_2750/LCC_IR1_MB_2750"
-#ADDRESS=$VISTRGB-$YESTERDAY
-array=($VISTRGB $IR1CR $IR1MB)
-
-for pictureType in "${array[@]}"; do
-    case ${pictureType} in 
-    ${VISTRGB})  FOLDER=$(echo "真實色");;
+#回波
+CV1TW="https://www.cwb.gov.tw/Data/radar/CV1_TW_3600_"
+ array=($VISTRGB $IR1CR $IR1MB $CV1TW)
+for chartType in "${array[@]}"; do
+    case ${chartType} in 
+    ${VISTRGB})  FOLDER=$(echo "真實色")
+                                ADDRESS=$(echo $chartType-$YESTERDAY);;
     ${IR1CR}) FOLDER=$(echo "紅外線");;
     ${IR1MB}) FOLDER=$(echo "色調強化");;
+    ${CV1TW}) FOLDER=$(echo "雷達回波")
+                        PICTURETYPE=$(echo ".png")
+                        YESTERDAYNODASH=$(date +"%Y%m%d" -d @$(($TIMESTAMP-86400)))
+                        ADDRESS=$(echo $chartType$YESTERDAYNODASH);;
     esac
-    ADDRESS=$pictureType-$YESTERDAY
+
     for i in {00..23}; do
     HOUR=$i
         for j in {00..50..10}; do
         MINUTE=$j
-        DATAADDRESS=$ADDRESS-$HOUR-$MINUTE$PICTURETYPE
-        wget   $DATAADDRESS -U $USERAGENT -P ./$YESTERDAY/$FOLDER
+        if [ "${chartType}" != "${CV1TW}" ]; then
+            DATAADDRESS=$ADDRESS-$HOUR-$MINUTE$PICTURETYPE
+            echo $DATAADDRESS
+        else
+             DATAADDRESS=$ADDRESS$HOUR$MINUTE$PICTURETYPE   
+         fi    
+        wget   $DATAADDRESS -U $USERAGENT -P $LOCAL$YESTERDAY/$FOLDER --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 --continue
         done
     done
 done
-
-/usr/bin/rclone copy  --ignore-existing --verbose --transfers 2 --checkers 10 --contimeout 60s --timeout 300s --retries 3  --stats 1s /home/mk/DD2/weatherChartDaily/$YESTERDAY console-share:每日氣象圖/$YESTERDAY
+rclone copy  --ignore-existing --verbose --transfers 2 --checkers 10 --contimeout 60s --timeout 300s --retries 3  --stats 1s $LOCAL$YESTERDAY console-share:每日氣象圖/$YESTERDAY
